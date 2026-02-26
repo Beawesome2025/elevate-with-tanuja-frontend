a9b4f225de00e070828798285c02bf250ce10f9f
Subhajit Mandal <subhajitm@Subhajits-MacBook-Air.local>
2026-02-27 02:31:42 +0530

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.r2.cloudflarestorage.com',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '1337',
+      },
+    ],
+  },
 };
-
 export default nextConfig;
